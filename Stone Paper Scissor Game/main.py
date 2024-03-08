6df147617caad8659bf4e02e64dbece21fefe9c5
Shreekrushna07 <160056017+Shreekrushna07@users.noreply.github.com>
2024-03-08 20:26:35 +0530

    Create main.py
    Rock, Paper, Scissors Game
    This is a simple command-line implementation of the classic Rock, Paper, Scissors game in Python. The game allows the player to compete against the computer in a series of matches.
    
    How to Play
    1. Run the script in a Python environment.
    2. Enter your choice: 0 for Rock, 1 for Paper, or 2 for Scissors.
    3. The computer will randomly select its choice.
    4. The winner is determined based on the choices:
        a. Rock beats Scissors
        b. Paper beats Rock
        c. Scissors beats Paper
        d. If both players choose the same, it's a tie.
    
    Features
    1. Uses ASCII art for visual representation of the choices.
    2. Implements basic input validation to ensure a valid choice is made.
    3. Randomizes the computer's choice for unpredictability.
    
    Technologies Used
    1. Python 3

---
+++ Stone Paper Scissor Game/main.py
@@ -0,0 +1,57 @@
+import random
+
+# Images for rock, paper, and scissors
+rock = '''
+    _______
+---'   ____)
+      (_____)
+      (_____)
+      (____)
+---.__(___)
+'''
+
+paper = '''
+    _______
+---'   ____)____
+          ______)
+          _______)
+         _______)
+---.__________)
+'''
+
+scissors = '''
+    _______
+---'   ____)____
+          ______)
+       __________)
+      (____)
+---.__(___)
+'''
+
+game_images = [rock, paper, scissors]
+
+# Get user's choice
+user_choice = int(input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors.\n"))
+
+# Validate user's choice
+if user_choice >= 3 or user_choice < 0: 
+    print("You typed an invalid number, you lose!")
+else:
+    print(game_images[user_choice])
+
+    # Get computer's choice
+    computer_choice = random.randint(0, 2)
+    print("Computer chose:")
+    print(game_images[computer_choice])
+
+    # Determine the winner
+    if user_choice == 0 and computer_choice == 2:
+        print("You win!")
+    elif computer_choice == 0 and user_choice == 2:
+        print("You lose")
+    elif computer_choice > user_choice:
+        print("You lose")
+    elif user_choice > computer_choice:
+        print("You win!")
+    elif computer_choice == user_choice:
+        print("It's a draw")
