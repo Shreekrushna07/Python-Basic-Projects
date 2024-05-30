96b0602c7aadcb3f2d628f705639b5569e86f09a
Shreekrushna Ravindra Dongare <160056017+Shreekrushna07@users.noreply.github.com>
2024-05-30 21:37:53 +0530

    Add files via upload

---
+++ Band Name Genrator/main.py
@@ -0,0 +1,15 @@
+# Band Name Genrator
+
+# Welcome message
+print("\nWelcome to the Band Name Generator.")
+
+# Accepting the city name from user
+print("\nWhat's name of city you grew up in?")
+city = input()
+
+# Accepting the pets name from user 
+print("\nWhat's your pet's name?")
+pet = input()
+
+# Genrating the band name
+print("\nYour band name could be " + city + " " + pet + ".")
